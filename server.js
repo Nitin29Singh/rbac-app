9cf80a1ce6a91765c1653065b535d4e17f93f32b
Nitin29Singh <nitin2004singhh29@gmail.com>
2025-10-31 14:59:28 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import protectedRoutes from "./routes/protectedRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/protected", protectedRoutes);
+
+app.use((req, res) => res.status(404).json({ message: "Route not found" }));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
